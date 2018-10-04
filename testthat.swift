60fc4c447b33fc8ce510508216e12ea9924955cc
Hunter Ford <hunter.ford05@gmail.com>
2018-10-04 19:54:24 -0400

    simplify implementation and test-writing
    - remove test name property
    - change `it(is:,if:)` to `returns(_:,if:)` to improve
      readability

--- testthat.swift
+++ testthat.swift
@@ -1,8 +1,11 @@
 import Foundation
 
+/// Shorthand for `TestThat` 
+public typealias TT = TestThat
+
 /// A minimalist testing tool with a focus on flexibility and intuitive
 ///  syntax.
-public class TT<Inputs,Result: Equatable>{
+public class TestThat<Inputs,Result: Equatable>{
 
   /// A `Test` is a closure that takes in generic `Inputs` and returns
   ///  a generic (but `Equatable`) `Result` 
@@ -12,46 +15,42 @@ public class TT<Inputs,Result: Equatable>{
   var test: Test 
   /// Test counter — useful for showing which test(s) fail
   var counter: Int = 0
-  /// Name of what is being tested (e.g. "foo()")
-  let name: String
 
   /**
-    Initializes a `TT` testing tool with the provided test case and
+    Initializes a `TestThat` testing tool with the provided test case and
      subject name.
 
     - Parameters:
-      - _: The name of whatever is being tested (e.g. "foo()")
       - when: The `Test` to run
 
-    - Returns: A shiny new `TT` testing tool instance
+    - Returns: A shiny new `TestThat` testing tool instance
   */
-  public init(_ name: String, when test: @escaping Test){
+  public init(_ test: @escaping Test){
     self.test = test
-    self.name = name 
   }
   
   /**
     Run a test.
 
-    Increments the test counter and performs the test with the given
-     inputs: if the result is incorrect, print an error message 
-     identifying the failing test as well as the expected and actual
-     outputs.
+    Run the test with the given inputs: if the result is incorrect,
+     print an error message identifying the failing test as well as the 
+     expected and actual outputs.
 
     - Parameters:
-      - is: The expected result
+      - _: The expected result
       - if: The input
 
-    - Returns: This `TT` instance
+    - Returns: This `TestThat` instance
   */
-  @discardableResult public func it(is expected: 
-   Result, if inputs: Inputs)->TT<Inputs,Result>{
+  @discardableResult public func returns(_ expected: 
+   Result, if inputs: Inputs)->TestThat<Inputs,Result>{
     counter += 1
     let result = self.test(inputs)
     if result != expected {
-      print("'\(name)' FAILED test #\(counter): " + 
+      print("FAILED test #\(counter): " + 
        "expected: '\(expected)', but got '\(result)'.")
     }
     return self
   }
 }
+
